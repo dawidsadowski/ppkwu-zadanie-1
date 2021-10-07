e3414622a0ece5fc5a67bf1317d30e56924714c4
dawidsadowski <dsadowski@craftware.biz>
2021-10-07 13:49:14 +0200

    Add Echo Get Mapping

--- src/main/java/com/example/ppkwuzadanie1/controller/RevController.java
+++ src/main/java/com/example/ppkwuzadanie1/controller/RevController.java
@@ -1,6 +1,7 @@
 package com.example.ppkwuzadanie1.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -8,8 +9,8 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/")
 public class RevController {
 
-    @GetMapping
-    String getRevText() {
-        return "Hello World!";
+    @GetMapping("{text}")
+    String getRevText(@PathVariable("text") String text) {
+        return text;
     }
 }
